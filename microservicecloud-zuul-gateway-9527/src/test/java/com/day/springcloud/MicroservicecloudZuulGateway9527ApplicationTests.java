77802ee8f1e3438d8e5e64afb98c41c0509318bb
DayByDay <1584589122@qq.com>
2020-04-27 22:08:52 +0800

    添加zuul

---
+++ microservicecloud-zuul-gateway-9527/src/test/java/com/day/springcloud/MicroservicecloudZuulGateway9527ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.day.springcloud;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicecloudZuulGateway9527ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
